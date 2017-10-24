ca1562c3272eb036cd383232e666f604c7908311
Thomas Pike <thomasp@opera.com>
2017-10-24 17:05:58 +0200

    Fix breakage on 404 error page

--- templates/error404.php
+++ templates/error404.php
@@ -17,4 +17,4 @@
 ?>
 <h1>Page not found</h1>
 <p>Sorry, but the address you've given doesn't seem to point to a valid page.</p>
-<p>If you got here by following a link, please <a href="mailto:<?php out($config['email']['admin_address'])?>?subject=<?php out('Broken link to '.$this->get('fulladdress').(empty($this->get('referrer')) ? '' : ' from '.$this->get('referrer')), ESC_URL_ALL)?>">report it to us</a>. Otherwise, please make sure that you have typed the address correctly, or just start browsing from the <a href="/">keys home page</a>.</p>
+<p>If you got here by following a link, please <a href="mailto:<?php out($this->get('admin_address'))?>?subject=<?php out('Broken link to '.$this->get('fulladdress').(empty($this->get('referrer')) ? '' : ' from '.$this->get('referrer')), ESC_URL_ALL)?>">report it to us</a>. Otherwise, please make sure that you have typed the address correctly, or just start browsing from the <a href="/">keys home page</a>.</p>
